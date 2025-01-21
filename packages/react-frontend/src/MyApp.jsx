23a90883b6ea042b3857b1df3aee0e1dfd2795e5
mallworth <maxallworthmiles@gmail.com>
2025-01-21 13:22:50 -0800

    POST working

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -11,8 +11,24 @@ export default function MyApp() {
       return promise;
     }
 
+    function postUser(person) {
+      const promise = fetch("http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(person)
+      });
+
+      return promise;
+    }
+
     function updateList(person) {
-      setCharacters([...characters, person]);
+      postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        });
     }
 
     function removeOneCharacter(index) {
